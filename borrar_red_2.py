ac34fb98a8df2ac7646465d94929f5593ef8e61a
stiffis <steve.ildefonso@utec.edu.pe>
2024-07-04 18:22:51 -0500

    archivos nuevos

--- borrar_red_2.py
+++ borrar_red_2.py
@@ -7,14 +7,22 @@ X = np.array([[0,0],
              [1,0],
              [1,1]])
 #Input
-Y =np.array([0,
-             1,
-             1,
-             0])#Output REAL
+Y =np.array([[0],#no olvidar el doble corchete
+             [1],
+             [1],
+             [0]])#Output REAL
 #First weight
-W = np.random.rand()#vacio por que solo te da un entero aleatorio
+weights1 = np.random.rand(2,2)#investigar más 
+weights2 = np.random.rand(2,1)
+
 #training parameters
     #epoch
-epochs = 1000 #no olvidar mencionar en la expo las epocas ‼️
+epochs = 10000 #no olvidar mencionar en la expo las epocas ‼️
     #learning rate
-learning_reate = 0.01
+learning_reate = 0.1
+
+for epoch in range(epochs):
+    for i in range(len(X)):
+        x_for_input = X[i].reshape(2,1)#2,1 por que la matriz tiene q ser vertical y no horizaontal
+        x_real = Y[i].reshape(1,1)#1,1 por que al inicio me devuelve una lista pero YO quiero una matriz de un solo elemento
+        
