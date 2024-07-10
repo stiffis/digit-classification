7bb457b071a1b3173cf67b866d72b96cf3fa63ea
stiffis <steve.ildefonso@utec.edu.pe>
2024-07-09 22:25:34 -0500

    nose

--- PROYECTO/agree.py
+++ PROYECTO/agree.py
@@ -5,31 +5,32 @@ import numpy as np
 # Cargar el conjunto de datos de dígitos
 digits = load_digits()
 
-novo = []
 digit = int(input("Ingrese un numero del 0 al 9: "))
 
-# Filtrar las imágenes donde digits.target es igual a digit
+# digits.images es un 3D NumpyArray donde cada elemento es de 2D representando a la imagen
+# digits.target ve todos los targets (numero correspondiente) de cada array
+# aqui se filtran las matrices del numero ingresado
 digit_images = digits.images[digits.target == digit]
-for x in digit_images:
-    novo.append(x)
 
-matrices_array = np.array(novo)
+# np.mean sirve para hallar el promedio de las imagenes
+# al ser un array 3D (numero de imagenes, altura, ancho)
+# axis significa que trabaja con el numero de imagenes
 average_matrix = np.mean(digit_images, axis=0)
+#al imprimir, me salian muchos numeros, se redondeo
 rounded_average_matrix = np.round(average_matrix)
+
+#este print no es necesario, solo esta para mejor compresion del resultado
 print(digit_images)
 print()
+#este si imprime la matriz promediada
 print(rounded_average_matrix)
 
-print(print(f"La cantidad de muestras para el num {digit} son {len(digit_images)}"))
+#este print tampoco es necesario
+print(f"La cantidad de muestras para el num {digit} son {len(digit_images)}")
 
+#####################################
+
+# aqui se muestra la imagen con matplotlib
 fig, ax = plt.subplots()
 c = ax.imshow(rounded_average_matrix, cmap='gray_r')
 plt.show()
-
-"""
-# mostrar con plt (matplotlib)
-plt.figure(figsize=(8, 6))
-
-# Mostrar la figura
-plt.show()
-"""
